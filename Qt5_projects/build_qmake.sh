6aeb449b277b0e053a554ad0b071b5c526308efc
whatmam <chs0517@hybus.net>
2022-03-02 06:04:34 +0000

    modify build_qmake.sh

--- Qt5_projects/build_qmake.sh
+++ Qt5_projects/build_qmake.sh
@@ -10,6 +10,7 @@ fi
 if [ -e ./${Project} ]; then
 	cp -rfvp ${Project} ${Project}_arm
 	cp -rfvp /opt/fsl-imx-wayland/5.10-hardknott/sysroots/x86_64-pokysdk-linux/usr/bin/qmake ${Project}_arm
+	find ${Project}_arm -type f -exec touch {} +
 else
 	echo "\"${1}\" Project file does not exist"
 	exit
